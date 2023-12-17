2bcbbd746f8298facb4616b825f64477c423151f
Vaibhav Dhingra <66358850+VaibhavDhingra24@users.noreply.github.com>
2023-12-17 19:00:09 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+// Converting numbers, strings to different operators
+
+let score = true
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; False => 0
+
+let isLoggedIn = "Vaibhav"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn)
+
+// 1 => True; 0 => False
+// "" => False
+// "Vaibhav" = True
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(typeof someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
