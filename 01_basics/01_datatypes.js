164b36c90e874a544f2ea7849673c3dc9b841816
Vaibhav Dhingra <66358850+VaibhavDhingra24@users.noreply.github.com>
2023-12-17 17:37:02 +0000

    datatypes

--- 01_basics/01_datatypes.js
+++ 01_basics/01_datatypes.js
@@ -1,1 +1,26 @@
 "Use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+3); console.log("Vaibhav") // avoid semicolons for better readability
+
+console.log(3+3)
+console.log("Vaibhav")
+
+let name = "Hitesh" // String datatypes
+let age = 24 // number datatypes
+// let is_logged_in = False // Boolean
+
+// Primitve datatypes
+
+// bigint for large number
+// string => '' or ""
+// boolean => True or False
+// null => standalone value
+// undefined => variable declared but value isnt defined eg. let state; - here the value isnt defined yet
+// symbol => (mostly used in react to find uniqueness in an individual component)
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
