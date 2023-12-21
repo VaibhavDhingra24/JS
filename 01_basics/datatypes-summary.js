64b80c7dbd015b704811e05b97f4e742250e546a
Vaibhav Dhingra <66358850+VaibhavDhingra24@users.noreply.github.com>
2023-12-21 07:58:20 +0000

    Memory types

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,4 +1,4 @@
-// # Primitive DataTypes - Call by value - changes that happen are in  a copy
+// # Primitive DataTypes - Call by value - changes that happen are in a copy of the original
 
 // 7 Types: String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
@@ -26,7 +26,7 @@ FOR TS const:number = 100 (We need to define in typescript)
 
 // # Reference Type(Non-primitive)
 
-// Arrays, Objects, Functions - Call by reference 
+// Arrays, Objects, Functions - Call by reference - Whatever changes we do, they reflect in the original as well
 
 const bros = ["Vaibhav", "Saatvik", "Lakshya", "Aashi", "Manan"] // Array => Square brackets
 
@@ -39,8 +39,39 @@ const myFunction = function(){
     console.log("Hello World");    // Function => function(){}
 }
 
-console.log(typeof bros) // Type shows object even though its an Array
-console.log(typeof myObj) // Object is object
-console.log(typeof myFunction) // Type shows function but its called 'Object Function'
+// console.log(typeof bros) // Type shows object even though its an Array
+// console.log(typeof myObj) // Object is object
+// console.log(typeof myFunction) // Type shows function but its called 'Object Function'
 
 // Typeof Operator documentation - https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+// Types of Memory 
+
+// Stack (Primitive datatypes), Heap (Non-primitive)
+ 
+let myYoutubeName = "NerdwithBeard" // string - primitive type hence stack
+
+let anothername = myYoutubeName 
+anothername = "vaibhavdhingra24"
+
+// console.log(anothername) // This shows the updated value
+// console.log(myYoutubeName) // This shows the origial value
+
+// This means that the 'anothername' thing takes a copy of the original and makes changes in that.
+
+let user1 = {
+    email: "User@gmail.com",  // This is an object so it goes in Heap memory
+    upi: "user@ybl"
+}
+
+// console.log(typeof user1)
+
+let user2 = user1
+user2.email = "vaibhav@gmail.com" // We update values in objects using '.' followed by the value we need to change
+
+// console.log(user1.email)
+// console.log(user2.email) // Here we see that both the objects return the same updated value,  which  means that changes in the original also happen unlike what we saw in the String(Primitive datatype(Stack memory))
+
